ccdcf799ca3dd773635fe530ea538c5e0bc4eb3e
gfdgd_xi <3025613752@qq.com>
2025-09-23 20:36:27 +0800

    新增riscv64模板
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

---
+++ iso-template/riscv64-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#xorriso -as mkisofs -R -r -J -joliet-long -l -cache-inodes -iso-level 3 -A "GXDE OS Live" -V "GXDE-OS" -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus -o gxde.iso loong64
+xorriso -as mkisofs -R -r -J -joliet-long -l -cache-inodes -iso-level 3 -A "GXDE OS Live" -p "https://www.gxde.top" -V "GXDE-OS" -e boot/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus -o gxde.iso riscv64
